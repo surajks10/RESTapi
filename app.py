1a28a8f0a11c0e2707d8221169e9540f3327dd4b
surajks10 <91935991+surajks10@users.noreply.github.com>
2024-07-02 19:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def delete_book(book_id):
     for book in books:
         if book['id'] == book_id:
             books.remove(book)
-            return "success",200
+            return jsonify({"message":"book removed successfully"}),200
     return jsonify({'error': 'Book not found'}), 404
 
 
